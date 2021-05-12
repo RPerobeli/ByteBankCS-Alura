69dcbba20a0ea8b7a7949610c108f394d4331861
RPerobeli <rodrigo.costa2015@engenharia.ufjf.br>
2021-05-12 11:07:49 -0300

    commit inicial

---
+++ ByteBank/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ContaCorrente conta = new ContaCorrente(7480, 874150);
+            //Console.WriteLine(ContaCorrente.TaxaOperacao);
+            TestaDivisao(0);
+            Console.ReadLine();
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            try
+            {
+                int resultado = Dividir(10, divisor);
+                Console.WriteLine("O resultado da divisao de 10 por " + divisor + " é " + resultado);
+            }
+            catch (DivideByZeroException erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Não é possivel dividir por 0!");
+            }
+            
+        }
+
+        private static int Dividir(int v, int divisor)
+        {
+            return v / divisor;
+        }
+    }
+    
+}
