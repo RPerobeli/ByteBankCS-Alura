8d223e399da04d4b570d237fa6939443f2e7b5a0
RPerobeli <rodrigo.costa2015@engenharia.ufjf.br>
2021-05-12 11:42:34 -0300

    feat(program): varios catchs

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -12,16 +12,14 @@ namespace ByteBank
         {
             //ContaCorrente conta = new ContaCorrente(7480, 874150);
             //Console.WriteLine(ContaCorrente.TaxaOperacao);
-            TestaDivisao(0);
-            Console.ReadLine();
-        }
-
-        private static void TestaDivisao(int divisor)
-        {
             try
             {
-                int resultado = Dividir(10, divisor);
-                Console.WriteLine("O resultado da divisao de 10 por " + divisor + " é " + resultado);
+                TestaDivisao(0);
+            }
+            catch(NullReferenceException erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
             }
             catch (DivideByZeroException erro)
             {
@@ -29,7 +27,14 @@ namespace ByteBank
                 Console.WriteLine(erro.StackTrace);
                 Console.WriteLine("Não é possivel dividir por 0!");
             }
-            
+
+            Console.ReadLine();
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("O resultado da divisao de 10 por " + divisor + " é " + resultado);          
         }
 
         private static int Dividir(int v, int divisor)
