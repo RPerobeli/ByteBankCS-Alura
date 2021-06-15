8dda532f8b9c1b680fb4bb8f9ba929864ef9d458
Rodrigo <rodrigo.costa2015@engenharia.ufjf.br>
2021-06-15 14:03:27 -0300

    doc(libs): documentacao da biblioteca de modelos do ByteBank

--- Parte 5/ByteBank2/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ Parte 5/ByteBank2/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -11,9 +11,19 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(345, 12345);
-            Console.WriteLine(conta.Numero);
+            //ContaCorrente conta = new ContaCorrente(345, 12345);
+            //Console.WriteLine(conta.Numero);
+            //Console.ReadLine();
+
+            DateTime dataFimPagamento = new DateTime(2021, 6, 15);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diff = dataFimPagamento - dataCorrente;
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diff);
+
+            Console.WriteLine(mensagem);
             Console.ReadLine();
+
         }
     }
 }
